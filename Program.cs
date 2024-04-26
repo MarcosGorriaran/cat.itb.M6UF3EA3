d7a14c9176f6bfa58a075e905d39d9116e7c8bb1
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-26 10:45:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using cat.itb.M6UF3EA2.helpers;
+using cat.itb.M6UF3EA3.helpers;
+
+namespace cat.itb.M6UF3EA3;
+
+public class Driver
+{
+    public static void Main()
+    {
+        const string MenuTitle = "Select one of the activities";
+        const string AskValue = "Provide me with the option index: ";
+        const string SplitText = ". ";
+        const string ExitText = "Exit";
+        const string ExitOption = "0";
+        Menu mainMenu = new Menu(MenuTitle, new Dictionary<string, string>()
+        {
+            {"1","Import colections"},
+            {"2","Drop collection"},
+            {"3A","Show book ISBN"},
+            {"3B","Show ordered books"},
+            {ExitOption,ExitText}
+        }, AskValue);
+        string option;
+
+        do
+        {
+            Console.Write(mainMenu.ToString(SplitText));
+            option = Console.ReadLine();
+
+            switch (option.Trim().ToUpper())
+            {
+                case "1":
+                    EA3CRUD.ACT1InsertFiles();
+                    break;
+                case "2":
+                    Console.Write("Target database: ");
+                    string database = Console.ReadLine();
+                    Console.Write("Target collection: ");
+                    string colection = Console.ReadLine();
+
+                    Console.WriteLine(EA3CRUD.dropCollection(database, colection));
+                    break;
+                case "3A":
+                    Console.WriteLine(EA3CRUD.ACT3AShowBookISBN());
+                    break;
+                case "3B":
+                    Console.WriteLine(EA3CRUD.ACT3BGetOrderedList());
+                    break;
+            }
+        } while (option != ExitOption);
+    }
+}
---
+++ helpers/EA3CRUD.cs
@@ -0,0 +1,122 @@
+﻿using cat.itb.M6UF3EA1.CRUD;
+using cat.itb.M6UF3EA1.Models;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using UF3_test.connections;
+
+namespace cat.itb.M6UF3EA3.helpers
+{
+    public static class EA3CRUD
+    {
+        public static void ACT1InsertFiles()
+        {
+            const string DB = "itb";
+            CRUDMongoDB<Book> bookCRUD = new CRUDMongoDB<Book>(DB, "book");
+            CRUDMongoDB<Product> productCRUD = new CRUDMongoDB<Product>(DB, "product");
+            bookCRUD.Insert(Book.ReadJSONArrayFile("FitxersJSON/books2.json"));
+            productCRUD.Insert(Product.ReadJSONArray(File.ReadAllText("FitxersJSON/products2.json").Split('\n')));
+        }
+        public static string dropCollection(string database, string collection)
+        {
+            IMongoCollection<BsonDocument> conn = MongoConnection.GetDatabase(database).GetCollection<BsonDocument>(collection);
+            string resultMsg = conn.CountDocuments(Builders<BsonDocument>.Filter.Empty) + " documents" + Environment.NewLine;
+            conn.Database.DropCollection(conn.CollectionNamespace.CollectionName);
+            resultMsg += "Remaining Collections: " + Environment.NewLine;
+            resultMsg += string.Join(Environment.NewLine, conn.Database.ListCollectionNames().ToList());
+            return resultMsg;
+        }
+        public static string ACT3AShowBookISBN()
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select();
+            foreach (Book book in books)
+            {
+                resultMsg += $"ISBN: {book.isbn}"+Environment.NewLine;
+            }
+            return resultMsg;
+        }
+        public static string ACT3BGetOrderedList()
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select().OrderByDescending(element=>element.pageCount);
+            foreach (Book book in books)
+            {
+                resultMsg += $"Títol: {book.title}"+Environment.NewLine+
+                    $"Categorias: ("+Environment.NewLine+
+                    $"{string.Join(Environment.NewLine,book.categories)}"+Environment.NewLine+
+                    $")"+Environment.NewLine;
+            }
+            return resultMsg;
+        }
+        public static string ACT3CShowDannoBooks(string searchAuthor)
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select(Builders<Book>.Filter.ElemMatch(element => element.authors, searchAuthor));
+            foreach (Book book in books)
+            {
+                resultMsg += $"Títol: {book.title}" + Environment.NewLine +
+                    $"Autores: (" + Environment.NewLine +
+                    $"{string.Join(Environment.NewLine, book.authors)}" + Environment.NewLine +
+                    $")" + Environment.NewLine;
+            }
+            return resultMsg;
+        }
+        public static string ACT3DFindSpecificJavaBooks(int minPage, int maxPage)
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select(Builders<Book>.Filter.ElemMatch(element => element.categories,"Java") & 
+                Builders<Book>.Filter.Gte(element=>element.pageCount,minPage) &
+                Builders<Book>.Filter.Lte(element=>element.pageCount,maxPage));
+            foreach (Book book in books)
+            {
+                resultMsg += $"Títol: {book.title}" + Environment.NewLine +
+                    $"Autores: (" + Environment.NewLine +
+                    $"{string.Join(Environment.NewLine, book.authors)}" + Environment.NewLine +
+                    $")" + Environment.NewLine+
+                    $"Paginas: {book.pageCount}"+Environment.NewLine;
+            }
+            return resultMsg;
+        }
+        public static string ACT3EShowBooks(params string[] authors)
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select();
+            books = books.Where(element =>
+            {
+                bool result = false;
+                foreach (string author in authors)
+                {
+                    if (element.authors.Contains(author)) return true;
+                }
+                return false;
+            });
+
+            foreach(Book book in books)
+            {
+                resultMsg += $"Títol: {book.title}" + Environment.NewLine +
+                    $"Autores: (" + Environment.NewLine +
+                    $"{string.Join(Environment.NewLine, book.authors)}" + Environment.NewLine +
+                    $")" + Environment.NewLine;
+            }
+            return resultMsg;
+        }
+        public static string ACT3FDiscardBooks(string searchCategory, string discardAuthor)
+        {
+            CRUDMongoDB<Book> crud = new CRUDMongoDB<Book>("book");
+            string resultMsg = string.Empty;
+            IEnumerable<Book> books = crud.Select(Builders<Book>.Filter.Not(Builders<Book>.Filter.ElemMatch(element=>element.authors, discardAuthor)) &
+                Builders<Book>.Filter.ElemMatch(element=>element.categories,searchCategory)).OrderBy(element=>element.title);
+            foreach(Book book in books)
+            {
+                resultMsg += book + Environment.NewLine;
+            }
+            return resultMsg;
+        }
+    }
+        
+}
